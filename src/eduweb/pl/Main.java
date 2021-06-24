39bfdda80e3232dbbc3fef324f713a660b8631b2
Grzegorz Świeboda <grzegorz.swieboda@sollers.eu>
2021-06-24 19:00:21 +0200

    Create methods printLetters and printBoard

--- src/eduweb/pl/Main.java
+++ src/eduweb/pl/Main.java
@@ -3,26 +3,38 @@ package eduweb.pl;
 public class Main {
 
     public static void main(String[] args) {
+        printLetters();
+        printBoard();
+    }
+
+    static void printLetters() {
         System.out.print("  ");
         for (int i = 0; i < 10; i++) {
             System.out.print((char) ('A' + i));
         }
 
         System.out.print('\n');
+    }
+
+    static void  printBoard() {
         for (int i = 1; i <= 10; i++) {
             if(i < 10)
                 System.out.print(' ');
             System.out.print(i);
 
             for (int j = 0; j < 10; j++) {
-                if(Math.random() < 0.2) {
-                    System.out.print('O');
-                } else {
-                    System.out.print(' ');
-                }
-
+                char shipValue = getRandomShip(Math.random());
+                System.out.print(shipValue);
             }
             System.out.print('\n');
+        }
+    }
+
+    private static char getRandomShip(double random) {
+        if(random < 0.2) {
+            return 'O';
+        } else {
+            return ' ';
         }
     }
 }
